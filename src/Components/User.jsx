f94a5d0fee7180cae0b2e4aa4b1a0bfd83c39449
Shravani Waghmale <shravaniwaghmale@gmail.com>
2024-03-13 21:46:48 +0530

    updating

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -9,17 +9,17 @@ function User({user}) {
             <div>
                 <img src={avatar_url} className="avatar" alt="User-img"/>
             </div>
-            <div>
+            <div className="name-cotainer">
                 <a href={`https://github.com/${login}`}>{name || login}</a>
                 <p>User joined on {`${createdDate.getDate()} ${createdDate.toLocaleString('en-us', {month : 'short'})} ${createdDate.getFullYear()}` }</p>
             </div>
-            <div>
+            <div className="profile-info">
                 <div>
                     <p>Public Repositories:</p>
                     <p>{public_repos}</p>
                 </div>
                 <div>
-                <p>Followers</p>
+                <p>Followers:</p>
                     <p>{followers}</p>
                 </div>
                 <div>
